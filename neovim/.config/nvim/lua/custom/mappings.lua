f946391b133ed8c1cc5dd39da1cdf4b0d0528149
Alejandro A. Moreno Sancho <aams.eam@gmail.com>
2024-04-04 10:23:02 +0200

    Add mappings to mappings.lua
    Some mappings where defined in plugins.lua, move them to mappings.lua

--- neovim/.config/nvim/lua/custom/mappings.lua
+++ neovim/.config/nvim/lua/custom/mappings.lua
@@ -36,5 +36,86 @@ M.telescope = {
       "Find Buffers" },
   }
 }
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<F5>"] = {
+      function()
+        require('dap.ext.vscode').load_launchjs() -- Load launch.json every time we debug
+        require('dap').continue()
+      end,
+      "Open debugging options/continue debugging"
+    },
+    ["<F6>"] = {
+      function()
+        require('dap').step_into()
+      end,
+      "Step into"
+    },
+    ["<F7>"] = {
+      function()
+        require('dap').step_over()
+      end,
+      "Step over"
+    },
+    ["<F8>"] = {
+      function()
+        require('dap').step_out()
+      end,
+      "Step out"
+    },
+    ["<leader>dG"] = {
+      function()
+        require('dap').run_to_cursor()
+      end,
+      "Run to cursor"
+    },
+    ["<leader>dsg"] = {
+      function ()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging gui"
+    },
+  }
+}
+
+M.persistent_breakpoints = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      function()
+        require("persistent-breakpoints.api").toggle_breakpoint()
+      end,
+      "Add persistent breakpoint at line"
+    },
+    ["<leader>dc"] = {
+      function()
+        require("persistent-breakpoints.api").set_conditional_breakpoint()
+      end,
+      "Add persistent conditional breakpoint at line"
+    },
+    ["<leader>du"] = {
+      function()
+        require("persistent-breakpoints.api").clear_all_breakpoints()
+      end,
+      "Clear all persistent breakpoints"
+    },
+  },
+}
+
+M.dap_python = {
+  n = {
+    ["<leader>dps"] = {
+      function()
+        require("dap-python").debug_selection()
+      end,
+      "Python debug selection"
+    },
+  }
+}
+
 return M
 
