ab54a29755387a46f93debbd7a0555cc1af83bed
Alejandro A. Moreno Sancho <56005012+aams-eam@users.noreply.github.com>
2024-05-03 09:21:20 +0200

    Add opacity config to wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,6 +7,7 @@ local config = {}
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.font = wezterm.font "UbuntuMono Nerd Font"
+config.window_background_opacity = 0.87
 config.font_size = 15
 config.keys = {
   {
